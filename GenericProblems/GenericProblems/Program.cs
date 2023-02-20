7b426633542f19b26d69e4d4fac77af9a4562b2a
Princekotadiya007 <princuuu77@gmail.com>
2023-02-20 09:50:40 +0530

    [prince]:add Maximum String

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -12,7 +12,7 @@ namespace GenericProblems
         {
             Console.WriteLine("Welcome to Maximum Problem Using Generics ");
             Console.WriteLine("choose the option");
-            Console.WriteLine("\n1.Maximum Integer, \n2. MaximumFloats");
+            Console.WriteLine("\n1.Maximum Integer, \n2. Maximum Floats, \n3. Maximum String");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -23,6 +23,13 @@ namespace GenericProblems
                 case 2:
                     Console.WriteLine("Maximum Floating Number");
                     Console.WriteLine(MaximumFloats.MaximumFloatsNumber(8.9, 10.2, 4.5));
+                    break;
+                case 3:
+                    Console.WriteLine("Maximum String Number");
+                    Console.WriteLine(MaximumString.FindString("prince", "vishal", "bhavin"));
+                    break;
+                default:
+                    Console.WriteLine("Invalied option");
                     break;
             }
         }

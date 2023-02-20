3f817fc0dbf39b05b1481d0f06eea37a87585b40
Princekotadiya007 <princuuu77@gmail.com>
2023-02-20 09:33:31 +0530

    [prince]:add Maximum Number Folat

---
+++ GenericProblems/GenericProblems/MaximumFloats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    public class MaximumFloats
+    {
+        public static double MaximumFloatsNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            //return value if all the numbers are equal
+            return firstValue;
+        }
+    }
+}
--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -12,12 +12,17 @@ namespace GenericProblems
         {
             Console.WriteLine("Welcome to Maximum Problem Using Generics ");
             Console.WriteLine("choose the option");
-            Console.WriteLine("\n1.Maximum Integer");
+            Console.WriteLine("\n1.Maximum Integer, \n2. MaximumFloats");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
+                    Console.WriteLine("Maximum Integer Number");
                     Console.WriteLine(MaximumInteger.MaximumNumbers(25, 48, 401));
+                    break;
+                case 2:
+                    Console.WriteLine("Maximum Floating Number");
+                    Console.WriteLine(MaximumFloats.MaximumFloatsNumber(8.9, 10.2, 4.5));
                     break;
             }
         }

ba99857fbdd72645092e096877d49f0a6c4cdd42
Princekotadiya007 <princuuu77@gmail.com>
2023-02-20 09:16:54 +0530

    [prince] : add maximum number

---
+++ GenericProblems/GenericProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Problem Using Generics ");
+            Console.WriteLine("choose the option");
+            Console.WriteLine("\n1.Maximum Integer");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine(MaximumInteger.MaximumNumbers(25, 48, 401));
+                    break;
+            }
+        }
+    }
+}

0e543b7582f0d2219527dbdb466ae78d7a7aa06b
Princekotadiya007 <princuuu77@gmail.com>
2023-02-21 18:22:25 +0530

    [prince]:add maximum number

--- FindsMaxUnits/UnitTest1.cs
+++ FindsMaxUnits/UnitTest1.cs
@@ -10,7 +10,13 @@ namespace FindsMaxUnits
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
         {
             int result = findsMax.IntNumber(20, 5, 10);
-            Assert.AreEqual(10, result);
+            Assert.AreEqual(20, result);
+        }
+        [Test]
+        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            int result = findsMax.IntNumber(15, 2, 12);
+            Assert.AreEqual(15, result);
         }
     }
 }
